cdb7b537452585ebaafead2653f69943dfa23fe2
Andrey Drozdov <drozdov_andrey@list.ru>
2022-07-07 11:16:37 +0300

    Task02 done

--- Task02/Program.cs
+++ Task02/Program.cs
@@ -2,4 +2,37 @@
 
 // [1,0,1,1,0,1,0,0]
 
+namespace GB  
 
+{
+    class Task2
+    {
+        static void Main(string[] args)
+        {
+        
+            Console.Clear();
+            int[] array = new int[8];
+            FillArray(array);
+            PrintArray(array);
+
+
+            void FillArray(int[] col)
+            {
+                for (int i = 0; i < col.Length; i++)
+                {
+                    col[i] = new Random().Next(0,2);
+                }
+            }
+
+            void PrintArray (int[] col)
+            {
+                for (int i = 0; i < col.Length; i++)
+                {
+                    Console.WriteLine(col[i]);
+                }
+            }
+           
+           
+        }  
+    }
+}
